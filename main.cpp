147e947c42f2b5f4fefd87ebee4687031f386a5e
John Mulder <john_c_mulder@yahoo.com>
2014-06-07 00:53:45 -0600

    Changed the name of the knapsack_solver to density_solver in order to match the way it works.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
+
 
 #include "knapsack.hpp"
-#include "knapsack_solver.hpp"
+#include "density_solver.hpp"
 #include "gloop_solver.hpp"
 
 using std::vector;
@@ -15,27 +18,52 @@ void test_solution(
 		size_t capacity, 
 		string name
 		);
+void run_simple_testcases(vector<string> names);
+void run_random_testcases(vector<string> names);
+
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	vector<string> names = {"knapsack", "gloop"};
-
-	for(string name : names)
-	{
-		// first simple test case
-		vector<knapsack_object> objects(10);
-		for(size_t i = 0; i < objects.size(); ++i)
-		{
-			objects[i].weight = i + 1;
-			objects[i].value = i;
-			objects[i].id = i;
-		}
-		size_t capacity = 15;
-
-		test_solution(objects, capacity, name);
-	}
-
+    srand(time(NULL));
+	vector<string> names = {"density", "gloop"};
+    // run_simple_testcases(names);
+    run_random_testcases(names);
 	return 0;
+}
+
+void run_simple_testcases(vector<string> names)
+{
+    for(string name : names)
+    {
+        // first simple test case
+        vector<knapsack_object> objects(10);
+        for(size_t i = 0; i < objects.size(); ++i)
+        {
+            objects[i].weight = i + 1;
+            objects[i].value = i;
+            objects[i].id = i;
+        }
+        size_t capacity = 15;
+
+        test_solution(objects, capacity, name);
+    }
+}
+
+void run_random_testcases(vector<string> names)
+{
+    vector<knapsack_object> objects(10);
+    for(size_t i = 0; i < objects.size(); ++i)
+    {
+        objects[i].weight = rand() % 10 + 1;
+        objects[i].value = rand() % 10 + 1;
+        objects[i].id = i;
+    }
+    size_t capacity = 15;
+    for(string name : names)
+    {   
+        auto o(objects);
+        test_solution(o, capacity, name);
+    }
 }
 
 void test_solution(
@@ -45,9 +73,9 @@ void test_solution(
 {
 	vector<knapsack_object> solution;
 
-	if(name == "knapsack")
+	if(name == "density")
 	{
-		solution = knapsack_solver(objects, capacity);
+		solution = density_solver(objects, capacity);
 	}
 	else if(name == "gloop")
 	{
